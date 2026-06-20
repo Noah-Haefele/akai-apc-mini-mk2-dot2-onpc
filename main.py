17a722d7158a8785a6c07e287753a37cfb46558f
Noah-Haefele <noah.haefele@gmx.de>
2026-06-20 11:37:44 +0200

    add led color constants and controller startup animation

--- main.py
+++ main.py
@@ -10,6 +10,19 @@ output_port_led = "APC mini mk2 1"
 input_port_button = "APC mini mk2 0"
 output_port_button = "loopThruAPC 3"
 
+MIDI_CHANNEL = 6
+# Colors
+OFF = 0
+RED = 5
+YELLOW = 13
+WHITE = 3
+
+# Animation
+ANIMATION_DELAY = 0.15
+LAST_PAD_DELAY = 0.05
+
+animation_sequence = [10, 14, 13, 12, 20, 29, 38, 46, 53, 44]
+
 
 # Midi
 def print_available_ports():
@@ -34,9 +47,31 @@ def open_ports():
         raise
 
 
+# Led
+def set_all_pads(midi_out, velocity):
+    for pad in range(64):
+        midi_out.send(mido.Message("note_on", channel=MIDI_CHANNEL, note=pad, velocity=velocity))
+
+def startup_animation(midi_out):
+    set_all_pads(midi_out, WHITE)
+    time.sleep(1)
+    midi_out.send(mido.Message("note_on", channel=MIDI_CHANNEL, note=10, velocity=YELLOW))
+    time.sleep(1)
+    for pad in animation_sequence[1:-1]:
+        midi_out.send(mido.Message("note_on", channel=MIDI_CHANNEL, note=pad, velocity=RED))
+        time.sleep(ANIMATION_DELAY)
+        midi_out.send(mido.Message("note_on", channel=MIDI_CHANNEL, note=pad, velocity=YELLOW))
+    time.sleep(LAST_PAD_DELAY)
+    midi_out.send(mido.Message("note_on", channel=MIDI_CHANNEL, note=animation_sequence[-1], velocity=YELLOW))
+    time.sleep(2)
+    set_all_pads(midi_out, OFF)
+
+
 # Main
 def main():
     midi_in_led, midi_out_led, midi_in_button, midi_out_button = open_ports()
+    startup_animation(midi_out_led)
+
     print("system running (idle)...")
     
     try:
